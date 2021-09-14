977660b83837b2a9eec866c9128511f99d9223fd
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-09-13 21:21:57 -0700

    Canged API and formating

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch')
 
 exports.handler = async function () {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokemon/ditto'
+    const POKE_API = 'https://api.fitbit.com/1/user/[user-id]/activities/date/[date].json'
+
     const response = await fetch (POKE_API)
     const data = await response.json()
 
