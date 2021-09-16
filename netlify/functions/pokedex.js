544ab55458126128585180867922e7a66d99e59c
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-09-15 21:46:47 -0700

    functioning page

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,21 @@
 const fetch = require('node-fetch')
+const POKE_API = 'https://type.fit/api/quotes'
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }  
 exports.handler = async function () {
-    const POKE_API = 'https://type.fit/api/quotes'
-
-    const response = await fetch (POKE_API)
-    const data = await response.json()
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data)
+    try{
+        const data = await fetch(POKE_API).then(response => response.json());
+        const randomInt = getRandomInt(data.length -1);
+        const randomQuote = data[randomInt];
+        console.log(randomQuote)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(randomQuote)
+        }
+    }catch(err){
+        console.log('-------------', err);
     }
+    
 }
